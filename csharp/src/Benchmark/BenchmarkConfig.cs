88ecebdb6fad25e29f08db98f15cfae558b0bee7
Yarin Omesi <yarinomesi@gmail.com>
2024-12-14 22:26:21 +0200

    Add Benchmark

---
+++ csharp/src/Benchmark/BenchmarkConfig.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
+
+namespace Benchmark;
+
+public class BenchmarkConfig : ManualConfig
+{
+    public BenchmarkConfig()
+    {
+        SummaryStyle = SummaryStyle.Default
+            .WithRatioStyle(RatioStyle.Percentage);
+
+        var baseJob = Job.Default;
+
+
+
+        AddJob(baseJob.WithArguments(new[] { new MsBuildArgument("/p:USE_NUGET=true") })
+            .WithId("3.29.0")
+            .WithBaseline(true)
+        );
+        AddJob(baseJob.WithArguments(new[] { new MsBuildArgument("/p:USE_NUGET=false") })
+            .WithId("improved-fixed-size-repeated")
+        );
+    }
+}
